9c6c82016d71af6c2bab900c7821cefe7e08c1d0
diendanx <diendanx@gmail.com>
2024-02-20 08:09:18 +0700

    change username and password for db connection

--- src/java/com/urekk/db/DBUtil.java
+++ src/java/com/urekk/db/DBUtil.java
@@ -12,7 +12,7 @@ public class DBUtil {
     private static Connection con;
     private static final String CLASS_NAME = "com.microsoft.sqlserver.jdbc.SQLServerDriver";
     private static final String USER = "sa";
-    private static final String PASSWORD = "trandinhtu";
+    private static final String PASSWORD = "123";
     private static final String CONNECTION_URL = "jdbc:sqlserver://localhost:1433;databaseName=Book_Store;encrypt=false";
     
     static {
